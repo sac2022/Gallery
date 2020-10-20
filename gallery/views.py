171b09b62eb57f54ec233c8e29c320bc7f9d8786
sac2022 <sac2022@users.noreply.github.com>
2020-10-21 01:54:03 +0530

    Views,Index Changed

--- gallery/views.py
+++ gallery/views.py
@@ -16,13 +16,13 @@ def index(request):
     else:
         form = ImageForm()
     all_img = ImageModel.objects.all()
-    print(request.GET.get('dropdown'))
-    img = ImageModel.objects.filter(category=request.GET.get('dropdown')).order_by('date').reverse()
+    print(request.GET.get('category'))
+    img = ImageModel.objects.filter(category=request.GET.get('category')).order_by('date').reverse()
     for i in all_img:
         if i is not cat_list:
             cat_list.append(i.category)
-            print(cat_list)
-    if request.GET.get('dropdown') is None:
-        return render(request, "index.html",{"all_img": all_img, "form": form, "cat_list": cat_list})
+            # print(cat_list)
+    if request.GET.get('category') is None:
+        return render(request, "index.html", {"all_img": all_img, "form": form, "cat_list": cat_list})
     else:
-        return render(request, "index.html",{"img": img, "form": form,"cat_list":cat_list})
+        return render(request, "index.html", {"img": img, "form": form, "cat_list": cat_list})
